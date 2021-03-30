10b84e148ce0716a080620c068770bdaee0a525d
Judasibe3991 <juan.sierra@zabud.com.co>
2021-03-29 21:10:33 -0500

    first model kt

---
+++ sociedad.androide/src/main/kotlin/com/restapi/sociedad/androide/domain/model/Person.kt
@@ -0,0 +1,23 @@
+package com.restapi.sociedad.androide.domain.model
+
+import java.time.LocalDate
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.Table
+
+import javax.persistence.GenerationType.AUTO
+
+@Entity
+@Table(name = "person")
+data class Person(
+    val dni: Long = 0,
+    val name: String = "",
+    val lastName: String = "",
+    val date: LocalDate? = null
+    ) {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    var id: Long = 0
+}
