9f37b29c530a944eb65428d3968ba450f3b94d1c
Juan David <juan.sierra@tul.com.co>
2021-04-27 13:50:06 -0500

    agregar proyecto de test

---
+++ demo-test/src/main/kotlin/com/example/demotest/DemoTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demotest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoTestApplication>(*args)
+}
